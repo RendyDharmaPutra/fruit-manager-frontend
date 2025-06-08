9df5fc2460512657b19a9e66a9eb35d797aa59ea
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2025-06-08 12:29:51 +0700

    update secret key

--- app/core/utils/auth/jwt_parse.ts
+++ app/core/utils/auth/jwt_parse.ts
@@ -3,7 +3,7 @@ import { jwtVerify } from "jose";
 export const jwtParse = async (token: string) => {
   const { payload } = await jwtVerify(
     token,
-    new TextEncoder().encode("SECRET")
+    new TextEncoder().encode(process.env.JWT_SECRET)
   );
 
   return payload;
